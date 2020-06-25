2313b9e21b1ea7797d0b76a4dcc10e64139ca0de
fzmolina <molina.ita.hd@gmail.com>
2020-06-25 12:36:54 -0400

    test

--- src/UDD.py
+++ src/UDD.py
@@ -50,6 +50,7 @@ def prod33(fte,prod):
             df_new = pd.concat([df_new,temp],axis=0)
 
         i += 1
+
  
     df_new.rename(columns={'date': 'Fecha', 'comuna': 'Comuna'}, inplace=True)
 
@@ -112,6 +113,48 @@ def prod33(fte,prod):
         data_t = reshaped.transpose()
         data_t.index.rename('', inplace=True)
         data_t.to_csv(prod + '-' + eachIM + '_T.csv')
+
+prod33Region(fte,producto):
+
+    df = pd.DataFrame()
+
+    i = 0
+    for file in glob.glob(fte + 'IM_region_completa_*.csv'):
+        print('Processing ' + file)
+        temp = pd.read_csv(file, sep=";", encoding="utf-8", decimal=".")
+
+        if i == 0:
+            df = temp
+        else:
+            df = pd.concat([df,temp],axis=0)
+
+        i += 1
+ 
+    df_new.rename(columns={'date': 'Fecha'}, inplace=True)
+
+    df_new['Fecha'] = pd.to_datetime(df['Fecha'], format='%Y%m%d').dt.strftime("%Y-%m-%d")
+
+
+
+    df.dropna(how='any', inplace=True)
+
+    df = normalizaNombreCodigoRegionYComuna(df)
+    df = insertSuperficiePoblacion(df)
+    #df.dropna(how='any', inplace=True)
+
+    df.drop_duplicates(inplace=True)
+
+    #Ordenamos las columnas
+    columns = ['Region', 'Codigo region', 'Comuna', 'Codigo comuna', 'Superficie_km2', 'Poblacion',
+               'IM_interno', 'IM_externo', 'IM', 'Fecha']
+    df = df[columns]
+    df.to_csv(prod + '.csv', index=False)
+
+    #try to build a single df with three rows per date per region
+    aux = df.melt(id_vars=['Region', 'Codigo region', 'Comuna', 'Codigo comuna', 'Superficie_km2', 'Poblacion', 'Fecha'],
+                  value_vars=['IM_interno', 'IM_externo', 'IM'])
+
+    aux.to_csv(prod + '_std.csv', index=False)
 
 
 if __name__ == '__main__':
