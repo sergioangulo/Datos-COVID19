cd1d60821bf6df2a7e2cb8108ba90070bfb8ed7a
dna33 <dna33@cornell.edu>
2021-02-10 17:39:34 -0300

    will create traffic info in the input

--- src/traffic.py
+++ src/traffic.py
@@ -22,9 +22,10 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 import json
-import os
 from urllib import request
 import sys
+import pandas as pd
+from datetime import datetime
 
 class traffic:
     def __init__(self, user, token,new_relic_user,new_relic_key):
@@ -33,6 +34,10 @@ class traffic:
         self.token = token
         self.new_relic_user = new_relic_user
         self.new_relic_key = new_relic_key
+        self.df_clones = pd.DataFrame(columns=['timestamp','count','uniques'])
+        self.df_views = pd.DataFrame(columns=['timestamp','count','uniques'])
+        self.now = pd.to_datetime(datetime.now()).strftime('%Y-%m-%d-%H:%M:%S')
+
 
     def lambda_handler(self):
         base_url = "https://api.github.com"
@@ -50,89 +55,17 @@ class traffic:
 
         for repo_name in ur:
 
-            print("Getting referral data for %s" % repo_name)
-            referrals = request.Request(
-                "%s/repos/%s/%s/traffic/popular/referrers" % (base_url, user, repo_name), headers={"Authorization": "token %s" % self.token}
-            )
-            referrals = request.urlopen(referrals)
-            referrals = json.load(referrals)
-            if len(referrals) > 0:
-                for ref in referrals:
-                    referred = {
-                        "eventType": "Referral",
-                        "repo_name": repo_name,
-                        "referrer": ref["referrer"],
-                        "count": ref["count"],
-                        "uniques": ref["uniques"],
-                    }
-                    print(referred)
-                    headers = {
-                        "X-Insert-Key": "%s" % self.new_relic_key,
-                    }
-                    req = request.Request(
-                        "https://insights-collector.newrelic.com/v1/accounts/%s/events"
-                        % self.new_relic_user,
-                        headers=headers,
-                        data=json.dumps(referred).encode("utf-8"),
-                    )
-                    resp = request.urlopen(req)
-                    print(resp.read())
-
-            print("Getting top referral path data for %s" % repo_name)
-
-            paths = request.Request(
-                "%s/repos/%s/%s/traffic/popular/paths" % (base_url, user, repo_name), headers={"Authorization": "token %s" % self.token}
-            )
-            paths = request.urlopen(paths)
-            paths = json.load(paths)
-            if len(paths) > 0:
-                for ref in paths:
-                    paths = {
-                        "eventType": "ReferralPath",
-                        "repo_name": repo_name,
-                        "path": ref["path"],
-                        "title": ref["title"],
-                        "count": ref["count"],
-                        "uniques": ref["uniques"],
-                    }
-                    print(paths)
-                    headers = {
-                        "X-Insert-Key": "%s" % self.new_relic_key,
-                    }
-                    req = request.Request(
-                        "https://insights-collector.newrelic.com/v1/accounts/%s/events"
-                        % self.new_relic_user,
-                        headers=headers,
-                        data=json.dumps(paths).encode("utf-8"),
-                    )
-                    resp = request.urlopen(req)
-                    print(resp.read())
-
             print("Getting views for %s" % repo_name)
             req = request.Request(
                 "%s/repos/%s/%s/traffic/views" % (base_url, user, repo_name), headers={"Authorization": "token %s" % self.token}
             )
             response = request.urlopen(req)
             req = json.load(response)
-            if req["count"] > 0 or req["uniques"] > 0:
-                viewed = {
-                    "eventType": "View",
-                    "repo_name": repo_name,
-                    "count": req["count"],
-                    "uniques": req["uniques"],
-                }
-                print(viewed)
-                headers = {
-                    "X-Insert-Key": "%s" % self.new_relic_key,
-                }
-                req = request.Request(
-                    "https://insights-collector.newrelic.com/v1/accounts/%s/events"
-                    % self.new_relic_user,
-                    headers=headers,
-                    data=json.dumps(viewed).encode("utf-8"),
-                )
-                resp = request.urlopen(req)
-                print(resp.read())
+            for i in req['views']:
+                dfi = pd.Series(i)
+                print(dfi)
+                self.df_views = self.df_views.append(dfi,ignore_index=True)
+
 
             print("Getting clones for %s" % repo_name)
             req = request.Request(
@@ -140,27 +73,78 @@ class traffic:
             )
             response = request.urlopen(req)
             req = json.load(response)
-            if req["count"] > 0 or req["uniques"] > 0:
-                cloned = {
-                    "eventType": "Clone",
-                    "repo_name": repo_name,
-                    "count": req["count"],
-                    "uniques": req["uniques"],
-                }
-                print(cloned)
-                headers = {
-                    "X-Insert-Key": "%s" % self.new_relic_key,
-                }
-                req = request.Request(
-                    "https://insights-collector.newrelic.com/v1/accounts/%s/events"
-                    % self.new_relic_user,
-                    headers=headers,
-                    data=json.dumps(cloned).encode("utf-8"),
-                )
-                resp = request.urlopen(req)
-                print(resp.read())
-            print(" ")
+            for i in req['clones']:
+                dfi = pd.Series(i)
+                print(dfi)
+                self.df_clones = self.df_clones.append(dfi,ignore_index=True)
 
+            # print("Getting referral data for %s" % repo_name)
+            # referrals = request.Request(
+            #     "%s/repos/%s/%s/traffic/popular/referrers" % (base_url, user, repo_name), headers={"Authorization": "token %s" % self.token}
+            # )
+            # referrals = request.urlopen(referrals)
+            # referrals = json.load(referrals)
+            # if len(referrals) > 0:
+            #     for ref in referrals:
+            #         referred = {
+            #             "eventType": "Referral",
+            #             "repo_name": repo_name,
+            #             "referrer": ref["referrer"],
+            #             "count": ref["count"],
+            #             "uniques": ref["uniques"],
+            #         }
+            #         print(referred)
+            #         headers = {
+            #             "X-Insert-Key": "%s" % self.new_relic_key,
+            #         }
+            #         req = request.Request(
+            #             "https://insights-collector.newrelic.com/v1/accounts/%s/events"
+            #             % self.new_relic_user,
+            #             headers=headers,
+            #             data=json.dumps(referred).encode("utf-8"),
+            #         )
+            #         resp = request.urlopen(req)
+            #         print(resp.read())
+            #
+            # print("Getting top referral path data for %s" % repo_name)
+            #
+            # paths = request.Request(
+            #     "%s/repos/%s/%s/traffic/popular/paths" % (base_url, user, repo_name), headers={"Authorization": "token %s" % self.token}
+            # )
+            # paths = request.urlopen(paths)
+            # paths = json.load(paths)
+            # if len(paths) > 0:
+            #     for ref in paths:
+            #         paths = {
+            #             "eventType": "ReferralPath",
+            #             "repo_name": repo_name,
+            #             "path": ref["path"],
+            #             "title": ref["title"],
+            #             "count": ref["count"],
+            #             "uniques": ref["uniques"],
+            #         }
+            #         print(paths)
+            #         headers = {
+            #             "X-Insert-Key": "%s" % self.new_relic_key,
+            #         }
+            #         req = request.Request(
+            #             "https://insights-collector.newrelic.com/v1/accounts/%s/events"
+            #             % self.new_relic_user,
+            #             headers=headers,
+            #             data=json.dumps(paths).encode("utf-8"),
+            #         )
+            #         resp = request.urlopen(req)
+            #         print(resp.read())
+            #
+
+    def save(self):
+        #views
+        self.df_views['timestamp'] = pd.to_datetime(self.df_views['timestamp'], format='%Y-%m-%d').dt.date
+        self.df_views.to_csv('../input/traffic/%s_views.csv' % self.now,index=False)
+
+        #clones
+        self.df_clones['timestamp'] = pd.to_datetime(self.df_clones['timestamp'], format='%Y-%m-%d').dt.date
+        self.df_clones.to_csv('../input/traffic/%s_clones.csv' % self.now,index=False)
 
 if __name__ == '__main__':
     my_user = sys.argv[2]
@@ -169,5 +153,6 @@ if __name__ == '__main__':
     my_newrelic_key = sys.argv[4]
     my_traffic = traffic(my_user,my_token,my_newrelic_user,my_newrelic_key)
     my_traffic.lambda_handler()
+    my_traffic.save()
 
 
