477b67caee5c6ae2b5b2511db78b5838676c9ee4
dna33 <Cruzado1982>
2021-03-03 22:31:59 -0300

    tweet tta

--- src/tweet.py
+++ src/tweet.py
@@ -94,6 +94,21 @@ def tweeting(consumer_key, consumer_secret, my_access_token, my_access_token_sec
                                                            # media4.media_id
                                                            ])
 
+    elif carrier == 'testeo':
+        tweet_text = "Actualicé los datos del informe de testeo y trazabilidad del @ministeriosalud de hoy 💫, ¡gracias @FunCienciayVida 😘! Mira específicamente qué actualicé en la imagen, y clónate el github https://github.com/MinCiencia/Datos-COVID19"
+        media1 = my_api.media_upload('./src/img/Datos covid_Bot_D_g1.png')
+        # media2= my_api.media_upload('./img/Datos covid_Bot_A_g2.png')
+        # media3= my_api.media_upload('./img/Datos covid_Bot_A_g3.png')
+        # media4= my_api.media_upload('./img/Datos covid_Bot_A_g4.png')
+
+        # Generate text tweet with media (image)
+        my_api.update_status(status=tweet_text, media_ids=[media1.media_id
+                                                           # media2.media_id,
+                                                           # media3.media_id,
+                                                           # media4.media_id
+                                                           ])
+
+
 if __name__ == '__main__':
 
     if len(sys.argv) == 6:
