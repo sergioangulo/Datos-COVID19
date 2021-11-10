8cac25e8c5cad10e20e6ce0181c755640ba0b270
dna33 <dna33@cornell.edu>
2021-11-10 14:48:57 -0300

    modifica código producto89 para calcular incidencia en vez de representar input. - corrección en caracter de decimales

--- src/vacunacion_casos.py
+++ src/vacunacion_casos.py
@@ -45,7 +45,7 @@ def prod89(fte, producto):
     df_output_file['incidencia_casos'] = ((df_output_file['casos_confirmados'] / df_output_file['poblacion'] ) * 100000).astype(float)
     df_output_file['incidencia_uci'] = ((df_output_file['casos_uci'] / df_output_file['poblacion'] ) * 100000).astype(float)
     df_output_file['incidencia_def'] = ((df_output_file['casos_def'] / df_output_file['poblacion'] ) * 100000).astype(float)
-    df_output_file.to_csv(producto, index=False, sep=',',decimal=',')
+    df_output_file.to_csv(producto, index=False, sep=',')
 
 
 def prod90(fte, producto):
