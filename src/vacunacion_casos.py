fe1deab88dba6f80c0af1acd252d08ee7f13c90d
dna33 <dna33@cornell.edu>
2021-11-10 14:40:20 -0300

    modifica código producto89 para calcular incidencia en vez de representar input. - corrección en incidencia defunciones

--- src/vacunacion_casos.py
+++ src/vacunacion_casos.py
@@ -42,9 +42,9 @@ import os.path
 
 def prod89(fte, producto):
     df_output_file = pd.read_csv(fte + 'incidencia_estado_vacunacion.csv',sep=';')
-    df_output_file['incidencia_casos'] = (df_output_file['casos_confirmados'] / df_output_file['poblacion'] ) * 100000
-    df_output_file['incidencia_uci'] = (df_output_file['casos_uci'] / df_output_file['poblacion'] ) * 100000
-    df_output_file['incidencia_def'] = (df_output_file['casos_uci'] / df_output_file['poblacion'] ) * 100000
+    df_output_file['incidencia_casos'] = ((df_output_file['casos_confirmados'] / df_output_file['poblacion'] ) * 100000).astype(float)
+    df_output_file['incidencia_uci'] = ((df_output_file['casos_uci'] / df_output_file['poblacion'] ) * 100000).astype(float)
+    df_output_file['incidencia_def'] = ((df_output_file['casos_def'] / df_output_file['poblacion'] ) * 100000).astype(float)
     df_output_file.to_csv(producto, index=False, sep=',',decimal=',')
 
 
