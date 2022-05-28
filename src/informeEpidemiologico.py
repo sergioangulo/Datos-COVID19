1cc69d8116232992ad5bbcc5cf62267e32861eea
Ignacio Acuna <ignacioacunafrias@gmail.com>
2022-05-28 13:01:40 -0400

    Dividir archvio semestral por tamaño en informe Epi

--- src/informeEpidemiologico.py
+++ src/informeEpidemiologico.py
@@ -233,7 +233,9 @@ def prod15(fte, prod):
     variables = [x for x in data_2022_1st.columns if x not in identifiers]
     df_std = pd.melt(data_2022_1st, id_vars=identifiers, value_vars=variables, var_name='Semana Epidemiologica',
                      value_name='Casos confirmados')
-    df_std.to_csv(prod + '_2022_1st_std.csv', index=False)
+    #df_std.to_csv(prod + '_2022_1st_std.csv', index=False)
+    df_std.iloc[:1500000].to_csv(prod + '_2022_1st_a_std.csv', index=False)
+    df_std.iloc[1500000:].to_csv(prod + '_2022_1st_b_std.csv', index=False)
 
     # normalization 2022 segunda mitad
     # data_2022_2nd = pd.concat(data_2022_2nd)
